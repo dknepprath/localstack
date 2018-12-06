56db57ca22d868facdc1ba6eb260d97165526c5a
David Knepprath <david.knepprath@cambiahealth.com>
2018-12-06 09:46:53 -0800

    WIP unit test

--- tests/integration/test_s3.py
+++ tests/integration/test_s3.py
@@ -85,6 +85,66 @@ def test_s3_put_object_notification():
     s3_client.delete_bucket(Bucket=TEST_BUCKET_WITH_NOTIFICATION)
 
 
+def test_s3_multipart_upload_notification():
+
+    s3_client = aws_stack.connect_to_service('s3')
+    sqs_client = aws_stack.connect_to_service('sqs')
+
+    key_by_path = 'key-by-hostname'
+    key_by_host = 'key-by-host'
+
+    # create test queue
+    queue_url = sqs_client.create_queue(QueueName=TEST_QUEUE_FOR_BUCKET_WITH_NOTIFICATION)['QueueUrl']
+    queue_attributes = sqs_client.get_queue_attributes(QueueUrl=queue_url, AttributeNames=['QueueArn'])
+
+    # create test bucket
+    s3_client.create_bucket(Bucket=TEST_BUCKET_WITH_NOTIFICATION)
+    s3_client.put_bucket_notification_configuration(Bucket=TEST_BUCKET_WITH_NOTIFICATION,
+                                                    NotificationConfiguration={'QueueConfigurations': [
+                                                        {'QueueArn': queue_attributes['Attributes']['QueueArn'],
+                                                         'Events': ['s3:ObjectCreated:*']}]})
+
+
+    part = 1
+    multipart_key = "multipart_object_new"
+    multipart_upload_parts = []
+    multipart_upload_dict = s3_client.create_multipart_upload(Bucket=test_bucket,
+                                                              Key=multipart_key)
+
+    uploadId = multipart_upload_dict['UploadId']
+
+    Blob data = Blob.valueOf('a'.repeat(5242880));
+
+    s3_client.upload_part(Bucket=test_bucket,
+                          Body=data.read(),
+                          Key=multipart_key,
+                          PartNumber=part,
+                          UploadId=multipart_upload_dict['UploadId'])
+
+    # put an object where the bucket_name is in the path
+    s3_client.put_object(Bucket=TEST_BUCKET_WITH_NOTIFICATION, Key=key_by_path, Body='something')
+
+    # put an object where the bucket_name is in the host
+    # it doesn't care about the authorization header as long as it's present
+    headers = {'Host': '{}.s3.amazonaws.com'.format(TEST_BUCKET_WITH_NOTIFICATION), 'authorization': 'some_token'}
+    url = '{}/{}'.format(os.getenv('TEST_S3_URL'), key_by_host)
+    # verify=False must be set as this test fails on travis because of an SSL error non-existent locally
+    response = requests.put(url, data='something else', headers=headers, verify=False)
+    assert response.ok
+
+    queue_attributes = sqs_client.get_queue_attributes(QueueUrl=queue_url,
+                                                       AttributeNames=['ApproximateNumberOfMessages'])
+    message_count = queue_attributes['Attributes']['ApproximateNumberOfMessages']
+    # the ApproximateNumberOfMessages attribute is a string
+    assert message_count == '2'
+
+    # clean up
+    sqs_client.delete_queue(QueueUrl=queue_url)
+    s3_client.delete_objects(Bucket=TEST_BUCKET_WITH_NOTIFICATION,
+                             Delete={'Objects': [{'Key': key_by_path}, {'Key': key_by_host}]})
+    s3_client.delete_bucket(Bucket=TEST_BUCKET_WITH_NOTIFICATION)
+
+
 def test_s3_get_response_default_content_type():
     # When no content type is provided by a PUT request
     # 'binary/octet-stream' should be used
